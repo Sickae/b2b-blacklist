90aa07596581a78f428f20af18ed6a74530d9c9c
Sipos Bence <bence.sipos@4sys.hu>
2022-05-30 19:13:18 +0200

    init

---
+++ src/lib/components/BlacklistGrid.svelte
@@ -0,0 +1,46 @@
+﻿<script context="module">
+    import * as contentful from "contentful";
+
+    const client = contentful.createClient({
+        space: "{{ spaceId }}",
+        accessToken: "{{ accessToken }}"
+    });
+</script>
+
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    let blacklists = [];
+    onMount(() => {
+        client.getEntries().then(response => {
+            console.log('response', response);
+            blacklists = [...response.items.map(item => item.fields)];
+        });
+    });
+    
+    $: console.log('bl', blacklists);
+</script>
+
+<div class="flex flex-col w-3/4 gap-4">
+    <div class="flex">
+        <input class="form-control input bg-base-200 w-full" placeholder="Search" />
+    </div>
+
+    <table class="table w-full shadow-md rounded-2xl">
+        <thead>
+        <tr>
+            <th>Character name</th>
+            <th>Reason</th>
+            <th>Date</th>
+        </tr>
+        </thead>
+        <tbody>
+        {#each blacklists as blacklist}
+            <tr>
+                <td>{blacklist.characterName}</td>
+                <td>{blacklist.reason}</td>
+                <td>{new Date(blacklist.date).toDateString()}</td>
+            </tr>
+        {/each}
+    </table>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+﻿<script lang="ts">
+    import '$lib/global.css';
+</script>
+
+<slot />
