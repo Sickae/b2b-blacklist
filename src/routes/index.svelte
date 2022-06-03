af51e47eccc65ca468106212879b2541dc245660
Sipos Bence <sipos.bnc@gmail.com>
2022-06-03 16:02:02 +0200

    title color changed to white for better contrast

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="bg-primary shadow-md flex items-center p-3">
-    <h1 class="mx-5 text-center w-full">Back2Basics - Guild Blacklist</h1>
+    <h1 class="mx-5 text-center w-full text-white">Back2Basics - Guild Blacklist</h1>
 </div>
 
 <div class="flex justify-center">
